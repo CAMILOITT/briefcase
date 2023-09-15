b5f79d447fc05a07caac35a8c5769aaaaab7229d
CAMILOITT <camilocamilo5@outlook.com>
2023-09-14 21:17:57 -0500

    add section and style

---
+++ src/const/projects.ts
@@ -0,0 +1,31 @@
+import pixelDraw from '../assets/img/pixel_art.png'
+import messageHub from '../assets/img/chatHub.png'
+import VideoCall from '../assets/img/videoConnect.png'
+import { Css, Express, MongoDb, Node, React, TypeScript } from './tech'
+
+// falta sockets
+const listProjects = [
+  {
+    title: 'Pixel Art',
+    description:
+      'Un sitio web de dibujo de pixel art que permite a los usuarios crear obras de arte detalladas pixel a pixel. Con una interfaz intuitiva, los artistas pueden dar vida a sus ideas en forma de arte pixelado. Además, ofrece la opción de exportar las creaciones en formatos populares para compartir fácilmente en línea o utilizar en proyectos',
+    img: pixelDraw,
+    tag: [TypeScript, React, Css],
+  },
+  {
+    title: 'Message Hub',
+    description:
+      'Un sitio web de mensajería que conecta a usuarios en tiempo real a través de chats individuales y grupales. Los usuarios pueden enviar mensajes de texto, imágenes, videos y documentos. Además, la plataforma ofrece notificaciones instantáneas para mantener a los usuarios actualizados. La seguridad de los datos es una prioridad, con cifrado de extremo a extremo para proteger la privacidad de las conversaciones.',
+    img: messageHub,
+    tag: [TypeScript, Css, React, Node, Express, MongoDb],
+  },
+  {
+    title: 'VideoCall',
+    description:
+      'Un sitio web de video llamadas que ofrece una experiencia de comunicación cara a cara en línea. Los usuarios pueden iniciar llamadas de video de alta calidad con amigos, familiares o colegas desde cualquier parte del mundo. La plataforma incluye funciones de chat, compartir pantalla y grabación de llamadas para una comunicación efectiva. Además, garantiza la seguridad de las conversaciones con encriptación de extremo a extremo.',
+    img: VideoCall,
+    tag: [TypeScript, Css, React, Node],
+  },
+]
+
+export default listProjects
---
+++ src/const/tech.ts
@@ -0,0 +1,25 @@
+export const Html = { name: 'HTML', icon: 'html', bg: '#dd4b25' }
+export const Pug = { name: 'PUG', icon: 'pug', bg: '#a87871' }
+export const Css = { name: 'CSS', icon: 'css', bg: '#0870c2' }
+export const Sass = { name: 'sass', icon: 'sass', bg: '#cf6c9c' }
+export const Tailwind = { name: 'Tailwind', icon: 'tailwind', bg: '#3ebff8' }
+export const Javascript = { name: 'JavaScript', icon: 'js', bg: '#f7e025' }
+export const TypeScript = { name: 'TypeScript', icon: 'ts', bg: '#377cc8' }
+export const React = { name: 'React', icon: 'react', bg: '#0cdaff' }
+export const Astro = { name: 'Astro', icon: 'astro', bg: '#ff5910' }
+export const Node = { name: 'Node', icon: 'nodejs', bg: '#86cf2f' }
+export const Express = { name: 'Express', icon: 'express', bg: '#393939' }
+export const MongoDb = { name: 'MongoDB', icon: 'mongodb', bg: '#199555' }
+export const PostgreSQL = {
+  name: 'postgreSQL',
+  icon: 'postgresql',
+  bg: '#376696',
+}
+export const Git = { name: 'Git', icon: 'git', bg: '#f05639' }
+export const GitHub = { name: 'GitHub', icon: 'github', bg: '#080808' }
+export const Figma = { name: 'Figma', icon: 'figma', bg: '#ff7667' }
+export const Illustration = {
+  name: 'Adobe Illustrator',
+  icon: 'adobe illustrator',
+  bg: '#390808',
+}
